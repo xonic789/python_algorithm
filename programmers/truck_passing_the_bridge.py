9e12266427d9a92746fe0fae4e14f0b48ce222ac
SeungWooL <xonic789@naver.com>
2021-05-03 22:29:59 +0900

    스택/큐

--- programmers/truck_passing_the_bridge.py
+++ programmers/truck_passing_the_bridge.py
@@ -1,20 +1,33 @@
 from collections import deque
-queue = deque()
-bridge_length = 2
-weight = 10
-truck_weights = deque([7, 4, 5, 6])
+truck_on_bridge = deque()
+complete = deque()
+bridge_length = 100
+weight = 100
+truck_weights = deque([10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
+truck_length = len(truck_weights)
 truck_weight_on_bridge = 0
 answer = 0
 
-while len(truck_weights) > 0:
+while len(complete) < truck_length:
     # 트럭 하나 출발
-    if weight >= truck_weight_on_bridge:
-        if 
-    truck_weight = truck_weights.popleft()
-    truck_weight_on_bridge += truck_weight
-    answer += 1
+
+    for i in range(bridge_length):
+        # 길이만큼 포문 작동
+        # 시간 추가
+        # 무게 확인
+        try:
+            temp = truck_weights[0]
+        except IndexError:
+            pass
+        if weight >= truck_weight_on_bridge + temp:
+            truck = truck_weights.popleft()
+            truck_weight_on_bridge += truck
+            truck_on_bridge.append(truck)
+        answer += 1
+    if len(truck_on_bridge) != 0:
+        complete_truck = truck_on_bridge.popleft()
+        truck_weight_on_bridge -= complete_truck
+        complete.append(complete_truck)
 
 
-
-    queue.append(truck_weight)
 
