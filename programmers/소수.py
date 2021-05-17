7febcff2ed3826400d04e58c51ffb04c1b33ff79
SeungWooL <xonic789@naver.com>
2021-05-17 23:33:48 +0900

    2021-05-17 알고리즘 커밋

--- programmers/소수.py
+++ programmers/소수.py
@@ -5,6 +5,7 @@ def demical(number):
     number = int(number)
     if number < 2: return False
     for i in range(2, number):
+
         if number % i == 0:
             return False
     return True
